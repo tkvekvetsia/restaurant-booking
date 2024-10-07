6019a743890239820b17f8147e5fd348e7e3abff
toma.kvekvetsia <toma.kvekvetsia@tera.ge>
2024-10-07 12:19:44 +0400

    feat(business):add business project

---
+++ apps/business/src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { EnvService, loadEnvFactory } from './core/services/env.service';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideClientHydration(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [EnvService],
+      useFactory: loadEnvFactory,
+    },
+  ],
+};
