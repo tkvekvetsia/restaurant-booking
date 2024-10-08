12164dd0fba1b03bcf4533b60d7eadd285e409d9
tkvekvetsia <kvekvetsiatoma@gmail.com>
2024-10-08 23:08:29 +0400

    feat(business):add sidenav

--- apps/business/src/app/app.component.ts
+++ apps/business/src/app/app.component.ts
@@ -4,17 +4,19 @@ import { Store } from '@ngrx/store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { selectIsLoggedIn } from '@restaurant-booking/auth';
 import { take } from 'rxjs';
+import { SideNavComponent } from './core/components/side-nav/side-nav.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SideNavComponent, NgClass],
   selector: 'rb-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   store = inject(Store);
-  public isLoggedIn = toSignal(this.store.select(selectIsLoggedIn).pipe(
-    take(1),
-  ));
+  public isLoggedIn = toSignal(
+    this.store.select(selectIsLoggedIn).pipe(take(1))
+  );
 }
