311da3fa303034848fb36213f84416c5a88110af
tkvekvetsia <kvekvetsiatoma@gmail.com>
2024-10-13 17:51:22 +0400

    feat(busines):uncoment routing

--- libs/my-restaurants/src/lib/lib.routes.ts
+++ libs/my-restaurants/src/lib/lib.routes.ts
@@ -2,18 +2,18 @@ import { Route } from '@angular/router';
 // import { MyRestaurantsComponent } from './components/my-restaurants/my-restaurants.component';
 
 export const myRestaurantsRoutes: Route[] = [
-  // {
-  //   path: '',
-  //   loadComponent: () =>
-  //     import('./components/my-restaurants/my-restaurants.component').then(
-  //       c => c.MyRestaurantsComponent
-  //     ),
-  // },
-  // {
-  //   path: 'add',
-  //   loadComponent: () =>
-  //     import('./components/add-restaurant/add-restaurant.component').then(
-  //       c => c.AddRestaurantComponent
-  //     ),
-  // },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/my-restaurants/my-restaurants.component').then(
+        c => c.MyRestaurantsComponent
+      ),
+  },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./components/add-restaurant/add-restaurant.component').then(
+        c => c.AddRestaurantComponent
+      ),
+  },
 ];
